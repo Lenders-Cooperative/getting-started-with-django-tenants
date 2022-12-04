42192bb030a8ffeff765d4a5b2ec6142cc63591f
Abhishek Rana <arana@thesummitgrp.com>
2022-12-04 22:06:56 +0530

    Adding Organization models for enabling multi tenancy

--- organizations/models.py
+++ organizations/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Organization(models.Model):
+    name = models.CharField(max_length=100)
+    id = models.CharField(max_length=100, unique=True)
+    details = models.CharField(max_length=100)
+
+
+class OrganizationModel(models.Model):
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
